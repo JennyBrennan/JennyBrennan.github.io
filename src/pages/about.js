3878335e40b5b5fa51c7564dc723a41f34cc9fcc
Jenny Brennan <jenhbrennan@gmail.com>
2020-07-04 10:32:38 +0100

    Remove unecessary imports from About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,6 @@
 import React from "react"
-import Header from "../components/header"
 import Layout from "../components/layout"
 import jennybrennan from "../images/jennybrennan.jpg"
-import twitter from "../images/twitter1.png"
-import linkedin from "../images/linkedin1.png"
-import email from "../images/email1.png"
 
 class AboutPage extends React.Component {
     render() {
