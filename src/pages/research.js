2b89326576666dfabd608bfdb06ffb11f2ba6d8b
Jenny Brennan <jenhbrennan@gmail.com>
2020-07-11 14:15:40 +0100

    Update various about me and research copy

--- src/pages/research.js
+++ src/pages/research.js
@@ -15,14 +15,14 @@ class ResearchPage extends React.Component {
                       <li>The use of algorithmic decision systems in the public sector</li>
                       <li>Methods and policy for auditing or regulatory inspection of algorithmic systems</li>
                     </ul>
-                    <p>I've also been involved with the Ada Lovelace Institute's research on facial recognition and biometrics.</p>
-                    <p>Beyond my main research, I'm also interested in the environmental impact of AI and existential risk.</p> {/* Add something on injustice/inequality */}
+                    <p>I'm also involved with the Ada Lovelace Institute's research on health data and inequality, facial recognition and biometrics.</p>
+                    <p>Beyond my main research, I'm also interested in the power dynamics of data infrastructure, the environmental impact of AI and existential risk.</p>
                     <p>I've previously worked on research into policymaking for future generations and  unconscious bias, particularly in recruitment and online platforms.</p>
                     <h2>Recent publications</h2>
                     <ul>
-                      <li><a href="https://www.adalovelaceinstitute.org/examining-the-black-box-tools-for-assessing-algorithmic-systems/">Examining the Black Box: Tools for Assessing Algorithmic Systems</a></li>
-                      <li>Facial recognition terms stuff</li>
-                      <li>Beyond Face Value</li>
+                      <li><a href="https://www.adalovelaceinstitute.org/examining-the-black-box-tools-for-assessing-algorithmic-systems/">Examining the black box: tools for assessing algorithmic systems</a></li>
+                      <li><a href="https://www.adalovelaceinstitute.org/facial-recognition-defining-terms-to-clarify-challenges/">Facial recognition: defining terms to clarify challenges</a></li>
+                      <li><a href="https://www.adalovelaceinstitute.org/beyond-face-value-public-attitudes-to-facial-recognition-technology/">Beyond face value: public attitudes to facial recognition technology</a></li>
                     </ul>
                 </article>
             </Layout>
