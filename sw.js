99adfa06b6582a329905849d7e51bba64931a385
Jenny Brennan <jenhbrennan@gmail.com>
2020-07-11 16:14:20 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e5ec578e8fefcee8dc89.js"
+    "url": "webpack-runtime-66ed31ca9bf56f4ee95c.js"
   },
   {
     "url": "framework-5e45b3789bc154636a84.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "68e9c5a0099425b479caf18c16a1b261"
+    "revision": "a00430da32d33f55db3f2ba9989f1924"
   },
   {
     "url": "polyfill-68528c3be76e45e09e78.js"
