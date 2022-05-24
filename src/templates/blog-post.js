c7c6247ca38133ca92d4eada899ad6dfd074a576
Jenny Brennan <jenhbrennan@gmail.com>
2022-05-24 21:37:18 +0100

    Migrate from gatsby-image to gatsby-plugin-image

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 import Metadata from "../components/metadata"
-import Img from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image";
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -12,12 +12,14 @@ class BlogPostTemplate extends React.Component {
    
     return (
       <Layout>
-        {post.frontmatter.featuredImage ? <Metadata title={post.frontmatter.title} desc={post.frontmatter.desc} canonical={canonicalUrl} image={post.frontmatter.featuredImage.childImageSharp.fluid.src}/>
+        {post.frontmatter.featuredImage ? <Metadata title={post.frontmatter.title} desc={post.frontmatter.desc} canonical={canonicalUrl} image={post.frontmatter.featuredImage.childImageSharp.gatsbyImageData.getSrc}/>
         : <Metadata title={post.frontmatter.title} desc={post.frontmatter.desc} canonical={canonicalUrl}/> }
         <article>
           {
             post.frontmatter.featuredImage &&  
-            <Img className="blog-robot" fluid={post.frontmatter.featuredImage.childImageSharp.fluid} />
+            <GatsbyImage
+              image={post.frontmatter.featuredImage.childImageSharp.gatsbyImageData}
+              className="blog-robot" />
           }
           <h1>{post.frontmatter.title}</h1>
           <h3 className="subheading">{post.frontmatter.desc}</h3>
@@ -32,38 +34,35 @@ class BlogPostTemplate extends React.Component {
           </main>
         </article>
       </Layout>
-    )
+    );
   }
 }
 
 export default BlogPostTemplate
 
-export const pageQuery = graphql`
-  query BlogPostBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-        author
-      }
+export const pageQuery = graphql`query BlogPostBySlug($slug: String!) {
+  site {
+    siteMetadata {
+      title
+      author
     }
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
-      html
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        desc
-        featuredImage {
-          childImageSharp {
-            fluid(maxWidth: 500) {
-              ...GatsbyImageSharpFluid_noBase64
-            }
-          }
+  }
+  markdownRemark(fields: {slug: {eq: $slug}}) {
+    id
+    html
+    fields {
+      slug
+    }
+    frontmatter {
+      title
+      date(formatString: "MMMM DD, YYYY")
+      desc
+      featuredImage {
+        childImageSharp {
+          gatsbyImageData(width: 500, placeholder: NONE, layout: CONSTRAINED)
         }
       }
     }
   }
+}
 `
